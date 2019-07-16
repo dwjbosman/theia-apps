65ebe99407ff9d09206c17af02cec4b882ed0475
Dinne Bosman <dwjbosman@yahoo.com>
2019-07-16 21:17:30 +0200

    empty bugs folder

--- theia-rust-docker/nvm_setup.sh
+++
@@ -1,6 +0,0 @@
-export NVM_DIR="/root/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-export PATH="/root/.yarn/bin:/root/.config/yarn/global/node_modules/.bin:$PATH"
-
--- theia-rust-docker/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-CURDIR=$(pwd)
-echo "container start path: $CONTAINER_START_PATH"
-echo "theia root dir: $CURDIR"
-env
-
-cd /root/theia_rust_extension
-yarn theia start $CURDIR --hostname=0.0.0.0 --log-level=debug --verbose
-
