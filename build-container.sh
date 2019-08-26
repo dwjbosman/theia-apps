f355c7f9941c43c1541850149843f7f5563d9a16
Dinne Bosman <dwjbosman@yahoo.com>
2019-08-26 23:29:46 +0200

    script debug

--- build-container.sh
+++ build-container.sh
@@ -6,15 +6,28 @@ NPM_TAG=$1
 IMAGE_NAME=$2
 NODEVERSION=$3
 
+
+# git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
+# git fetch
+# git diff --name-only HEAD origin/${TRAVIS_BRANCH}
+
+git remote set-branches --add origin $TRAVIS_BRANCH
+git fetch
+
 cd "$IMAGE_NAME-docker"
 
-CHANGED_FILES=$(git diff --name-status HEAD~1 HEAD .)
+#CHANGED_FILES=$(git diff --name-status HEAD~1 HEAD .)
+CHANGED_FILES=$(git diff --name-status HEAD...$TRAVIS_BRANCH .)
 if [ -z "$CHANGED_FILES" ]
 then
     # nothing changed, skip building
     exit 0    
 fi
 echo "There were changes in $IMAGE_NAME"
+echo $CHANGED_FILES
+
+exit 0
+
 
 IMAGE="theiaide/$IMAGE_NAME"
 IMAGE_TAG="$IMAGE":$(npm view "@theia/core@$NPM_TAG" version)
